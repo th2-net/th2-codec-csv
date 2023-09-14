5ac5d88ff1d0899dc888fd222c08d6dc93d84c3a
Oleg Smelov <45400511+lumber1000@users.noreply.github.com>
2023-09-14 17:26:18 +0400

    transport protocol, book&page, gRPC (#29)
    * migration to book and pages
    * th2 transport protocol support
    * gRPC connection support

--- src/main/java/com/exactpro/th2/codec/csv/CodecFactory.java
+++ src/main/java/com/exactpro/th2/codec/csv/CodecFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Exactpro (Exactpro Systems Limited)
+ * Copyright 2023 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,14 +32,7 @@ import com.google.auto.service.AutoService;
 
 @AutoService(IPipelineCodecFactory.class)
 public class CodecFactory implements IPipelineCodecFactory {
-    public static final String PROTOCOL = "csv";
-    private static final Set<String> PROTOCOLS = Collections.singleton(PROTOCOL);
-
-    @NotNull
-    @Override
-    public String getProtocol() {
-        return PROTOCOL;
-    }
+    private static final Set<String> PROTOCOLS = Collections.singleton("csv");
 
     @NotNull
     @Override
